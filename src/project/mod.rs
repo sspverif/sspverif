42b618b8340aeeffc2383a72d47c7c115161d255
Christoph Egger <egger@irif.fr>
2025-09-05 17:38:41 +0200

    clippy

--- src/project/mod.rs
+++ src/project/mod.rs
@@ -18,7 +18,6 @@ use crate::parser::SspParser;
 use crate::{
     gamehops::{equivalence, GameHop},
     package::{Composition, Package},
-    proof::Proof,
     theorem::Theorem,
     transforms::Transformation,
     util::prover_process::ProverBackend,
