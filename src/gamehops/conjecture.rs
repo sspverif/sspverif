985bd848ed2bfe296ba3405c0409509726c58e04
Christoph Egger <egger@irif.fr>
2025-09-06 12:14:46 +0200

    Parse Conjecture

--- src/gamehops/conjecture.rs
+++ src/gamehops/conjecture.rs
@@ -7,6 +7,12 @@ pub(crate) struct Conjecture<'a> {
 }
 
 impl<'a> Conjecture<'a> {
+    pub fn new(left_game: GameInstanceName<'a>, right_game: GameInstanceName<'a>) -> Self {
+        Self {
+            left_game,
+            right_game,
+        }
+    }
     pub(crate) fn left_name(&self) -> &GameInstanceName<'a> {
         &self.left_game
     }
